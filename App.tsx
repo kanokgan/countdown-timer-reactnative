bc515e06f9a4bd09aa2729bc477c77fd67883358
Kanokgan Hanmungtham <kanokgan@gmail.com>
2020-05-27 08:43:37 +0700

    Hello World

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={{
+      flex: 1,
+      justifyContent: "center",
+      alignContent: "center",
+    }}>
+      <Text>Hello World</Text>
+    </View>
+  );
+}
